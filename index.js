7974a0d25b92b856d466a0e146b36ace0ecb6ff7
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-07-01 12:28:31 +0600

    async function and try catch , finally applied

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from todo World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxxcucw.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+      await client.connect();
+      const taskCollection = client.db("todo_list").collection("task");
+
+      app.get('/task', async(req, res) => {
+        const query = {};
+        const cursor = taskCollection.find(query);
+        const tasks = await cursor.toArray();
+        res.send(tasks);
+      })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from todo World!");
+});
 
 app.listen(port, () => {
-  console.log(`Todo app listening on port ${port}`)
-})
+  console.log(`Todo app listening on port ${port}`);
+});
