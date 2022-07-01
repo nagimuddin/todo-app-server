8964f393b3ce98736fa7d49270c6fda28bf6f16e
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-07-02 05:23:56 +0600

    server site update

--- index.js
+++ index.js
@@ -9,21 +9,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxxcucw.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.55fmber.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
       await client.connect();
       const taskCollection = client.db("todo_list").collection("task");
 
       app.get('/task', async(req, res) => {
-        const query = {};
-        const cursor = taskCollection.find(query);
-        const tasks = await cursor.toArray();
-        res.send(tasks);
+        const newTask = req.body;
+        console.log('adding new task', newTask);
+        const result = await taskCollection.insertAll(newTask);
+        res.send(result);
       })
+
+      // Get Tools
+      app.get('/task', async (req, res) => {
+        const query = {};
+        const task = await taskCollection.find(query).toArray();
+        res.send(task);
+    })
+
+    // Get Tools
+    app.post('/task', async (req, res) => {
+      const task = req.body;
+      const result = await taskCollection.insertOne(task);
+      res.send({ success: true });
+  })
+
+
+
     }
     finally{
 
