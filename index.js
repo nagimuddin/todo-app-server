6747f0f63b5bfd3d568e4831852cc9f9cb029870
nagimuddin <97009292+nagimuddin@users.noreply.github.com>
2022-07-02 08:44:51 +0600

    operation success

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -10,11 +11,13 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.55fmber.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
 async function run() {
   try {
     await client.connect();
@@ -28,13 +31,14 @@ async function run() {
       res.send(task);
     });
 
-    // Get Tools
+    // Get Task
     app.post("/task", async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
       res.send(result);
     });
-    //delete Tools
+
+    //delete Task
     app.delete("/task/:id", async (req, res) => {
       const id = req.params.id;
       const result = await taskCollection.deleteOne({ _id: ObjectId(id) });
